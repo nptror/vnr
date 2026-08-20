01b7406be45a6cff665a976d65f8b2bc0ad83d58
thinh <thinhtnhse184770@fpt.edu.vn>
2026-08-20 12:49:09 +0700

    feat: add new meme assets and implement configuration file for meme categories

--- vnr-spst/src/config/memes.js
+++ vnr-spst/src/config/memes.js
@@ -1,8 +1,12 @@
+import catLaugh from '../assets/meme/fun/gif_fun/Cat Laugh GIF.gif'
+import dogLaughing from '../assets/meme/fun/gif_fun/Dog Laughing GIF.gif'
+import lerolero from '../assets/meme/fun/gif_fun/Lerolero Otag Sticker.gif'
 import sad1 from '../assets/meme/sad/kindpng_1046489.png'
 import suprise1 from '../assets/meme/suprise/kindpng_1290620.png'
 import suprise2 from '../assets/meme/suprise/kindpng_3708768.png'
 import other1 from '../assets/meme/other/kindpng_7159180.png'
 import other2 from '../assets/meme/other/pngwing.com.png'
+import catGive from '../assets/meme/other/gif_orther/Cat Give GIF.gif'
 
 /**
  * Danh sách meme theo folder.
@@ -10,6 +14,14 @@ import other2 from '../assets/meme/other/pngwing.com.png'
  * memes: array meme thuộc folder đó
  */
 const MEME_FOLDERS = [
+  {
+    folder: { id: 'fun', name: 'Vui nhộn', icon: '😂' },
+    memes: [
+      { id: 'fun_cat_laugh', label: 'Mèo cười', file: catLaugh },
+      { id: 'fun_dog_laugh', label: 'Chó cười', file: dogLaughing },
+      { id: 'fun_lerolero', label: 'Lerolero', file: lerolero },
+    ],
+  },
   {
     folder: { id: 'sad', name: 'Buồn bã', icon: '😢' },
     memes: [
@@ -26,6 +38,7 @@ const MEME_FOLDERS = [
   {
     folder: { id: 'other', name: 'Khác', icon: '🙂' },
     memes: [
+      { id: 'other_cat_give', label: 'Mèo đưa', file: catGive },
       { id: 'other1', label: 'Khác 1', file: other1 },
       { id: 'other2', label: 'Khác 2', file: other2 },
     ],
